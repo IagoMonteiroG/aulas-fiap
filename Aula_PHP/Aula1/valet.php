a0d764a4c9bd0669d2376d68bbbbee923303de7f
Logon Aluno <logonaluno@local.com>
2020-02-14 22:29:58 -0300

    finaliza

--- Aula_PHP/Aula1/valet.php
+++ Aula_PHP/Aula1/valet.php
@@ -1,4 +1,7 @@
 <?php
+
+//php -S localhost:8080
+
 require_once("funcoe.php");
 
 $veiculos = [
@@ -16,10 +19,27 @@ adicionaVeiculo($veiculos, "PHP-001", "Charger", "Dodge");
 $veiculos = removeVeiculo($veiculos, "ABC-1234");
 
 // print_r($veiculos);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Fiap Estacionamento</title>
+</head>
+<body>
+    <ul>
+        <?php foreach($veiculos as $placa => $veiculo){ ?>
+            <li><?= echo $placa . " " . $veiculo["marca"] . " "
+                . $veiculo["modelo"]; ?></li>
+        <?php } ?>
 
-foreach($veiculos as $placa => $veiculo){
-    echo $placa . " " . $veiculo["marca"] . " "
-    . $veiculo["modelo"] . "<br>";
+    </ul>
+</body>
+</html>
+
+
+    
 }
 
-//php -S localhost:8080
